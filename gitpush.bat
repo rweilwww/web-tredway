cdf5a8920a8e58651cf4cd81093fc1d2cf738b21
rweilwww <github@shaw-weil.com>
2025-11-03 19:05:43 -0500

    from inDesign 2025-11-04 00:05

--- gitpush.bat
+++ gitpush.bat
@@ -3,11 +3,10 @@ cd /inetpub/wwwroot/w/tredway
 
 
 git add .
-git commit -m "from inDesign 2025-10-25 11:32" --no-edit
+git commit -m "from inDesign 2025-11-04 00:05" --no-edit
 git push --force --set-upstream origin main
 
 rem wait for one minute to allow git hub to get its act together
 timeout 60
-"C:\Program Files\Google\Chrome\Application\chrome.exe" https://edit.shaw-weil.com/fix/?task=updateWebStatus&f=extreacted&fv=2025-10-25&w=htmTrailId&wv=tredway
 timeout 5
 exit
